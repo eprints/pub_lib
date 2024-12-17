aab5efa38bff10c2f8431df6840d80cf1efa8098
David R Newman <drn@ecs.soton.ac.uk>
2024-12-17 17:20:25 +0000

    Addresses eprints/eprints3.5#70

--- defaultcfg/cfg.d/eprint_fields_archive.pl
+++ defaultcfg/cfg.d/eprint_fields_archive.pl
@@ -1,13 +1,9 @@
 
 push @{$c->{fields}->{eprint}},
 
-{ 
-	name => "contact_email", 
-	type => "email", 
-	required => 0, 
-	can_clone => 0, 
-	export_as_xml => 0,
-},
+#{
+#	Add archive specific fields here.  To override an existing field use 'replace_core => 1'
+#},
 
 ;
 
@@ -15,28 +11,28 @@ push @{$c->{fields}->{eprint}},
 
 =for COPYRIGHT BEGIN
 
-Copyright 2022 University of Southampton.
-EPrints 3.4 is supplied by EPrints Services.
+Copyright 2024 University of Southampton.
+EPrints 3.5 is supplied by EPrints Services.
 
-http://www.eprints.org/eprints-3.4/
+http://www.eprints.org/eprints-3.5/
 
 =for COPYRIGHT END
 
 =for LICENSE BEGIN
 
-This file is part of EPrints 3.4 L<http://www.eprints.org/>.
+This file is part of EPrints 3.5 L<http://www.eprints.org/>.
 
-EPrints 3.4 and this file are released under the terms of the
+EPrints 3.5 and this file are released under the terms of the
 GNU Lesser General Public License version 3 as published by
 the Free Software Foundation unless otherwise stated.
 
-EPrints 3.4 is distributed in the hope that it will be useful,
+EPrints 3.5 is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public
-License along with EPrints 3.4.
+License along with EPrints 3.5.
 If not, see L<http://www.gnu.org/licenses/>.
 
 =for LICENSE END

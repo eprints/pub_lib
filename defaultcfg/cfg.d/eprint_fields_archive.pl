8080780e9444a9f2cf138e8adaa363e81c38c28d
David R Newman <drn@ecs.soton.ac.uk>
2025-04-19 17:56:34 +0000

    Fixes useless push syntax warning

--- defaultcfg/cfg.d/eprint_fields_archive.pl
+++ defaultcfg/cfg.d/eprint_fields_archive.pl
@@ -1,8 +1,8 @@
 
-push @{$c->{fields}->{eprint}},
+#push @{$c->{fields}->{eprint}},
 
 #{
-#	Add archive specific fields here.  To override an existing field use 'replace_core => 1'
+#	Add archive specific fields here, after uncommenting push line above.  To override an existing field use 'replace_core => 1'
 #},
 
 ;

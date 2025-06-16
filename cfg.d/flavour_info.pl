c544f333aeea1ea7b1929771e903176d088cc6ba
David R Newman <drn@ecs.soton.ac.uk>
2025-06-16 20:21:24 +0000

    Updates version in pub_lib's flavour_info.pl

--- cfg.d/flavour_info.pl
+++ cfg.d/flavour_info.pl
@@ -1,36 +1,36 @@
 $c->{flavour_id} = "pub";
 $c->{flavour_name} = "Publication";
-$c->{flavour_version} = "2.0";
+$c->{flavour_version} = "3.5.0";
 
-$c->{version_alias} = "TBD";
+$c->{version_alias} = "Highfield Hippogriff";
 
 =head1 COPYRIGHT
 
 =for COPYRIGHT BEGIN
 
-Copyright 2023 University of Southampton.
-EPrints 3.4 is supplied by EPrints Services.
+Copyright 2025 University of Southampton.
+EPrints 3.5 Beta 1 is supplied by EPrints Services.
 
-http://www.eprints.org/eprints-3.4/
+https://github.com/eprints/eprint3.5
 
 =for COPYRIGHT END
 
 =for LICENSE BEGIN
 
-This file is part of EPrints 3.4 L<http://www.eprints.org/>.
+This file is part of EPrints 3.5 Beta 1 L<https://www.eprints.org/>.
 
-EPrints 3.4 and this file are released under the terms of the
+EPrints 3.5 Beta 1 and this file are released under the terms of the
 GNU Lesser General Public License version 3 as published by
 the Free Software Foundation unless otherwise stated.
 
-EPrints 3.4 is distributed in the hope that it will be useful,
+EPrints 3.5 Beta 1 is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public
-License along with EPrints 3.4.
-If not, see L<http://www.gnu.org/licenses/>.
+License along with EPrints 3.5 Beta 1.
+If not, see L<https://www.gnu.org/licenses/>.
 
 =for LICENSE END
 

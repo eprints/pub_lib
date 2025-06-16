8c82231610ee934125ec39a60d4f7a829793ca85
David R Newman <drn@ecs.soton.ac.uk>
2025-06-16 20:30:09 +0000

    Fixes typo for version in copyright/licence boilerplate

--- cfg.d/flavour_info.pl
+++ cfg.d/flavour_info.pl
@@ -9,7 +9,7 @@ $c->{version_alias} = "Highfield Hippogriff";
 =for COPYRIGHT BEGIN
 
 Copyright 2025 University of Southampton.
-EPrints 3.5 Beta 1 is supplied by EPrints Services.
+EPrints 3.5.0 Beta 1 is supplied by EPrints Services.
 
 https://github.com/eprints/eprint3.5
 
@@ -17,19 +17,19 @@ https://github.com/eprints/eprint3.5
 
 =for LICENSE BEGIN
 
-This file is part of EPrints 3.5 Beta 1 L<https://www.eprints.org/>.
+This file is part of EPrints 3.5.0 Beta 1 L<https://www.eprints.org/>.
 
-EPrints 3.5 Beta 1 and this file are released under the terms of the
+EPrints 3.5.0 Beta 1 and this file are released under the terms of the
 GNU Lesser General Public License version 3 as published by
 the Free Software Foundation unless otherwise stated.
 
-EPrints 3.5 Beta 1 is distributed in the hope that it will be useful,
+EPrints 3.5.0 Beta 1 is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public
-License along with EPrints 3.5 Beta 1.
+License along with EPrints 3.5.0 Beta 1.
 If not, see L<https://www.gnu.org/licenses/>.
 
 =for LICENSE END
